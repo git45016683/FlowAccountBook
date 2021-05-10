418658aca430100d01c31d90f761cdede38a19ca
icemaple <450196683@qq.com>
2021-05-11 00:56:18 +0800

    添加底部导航栏； 添加底部导航栏图标资源

--- flowaccountbook/lib/main.dart
+++ flowaccountbook/lib/main.dart
@@ -1,113 +1,134 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
+main(List<String> args) {
+  return runApp(FAbook());
 }
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+var _unselectColor = Colors.blueGrey;
+var _selectColor = Colors.red;
+var _selectIconSize = 28.0;
+
+Map<String, List<String>> imageUrlMap = {
+  "首页": [
+    "assets/images/ic_home_nav_property.png",
+    "assets/images/ic_home_nav_property_pressed.png"
+  ],
+  "账单": [
+    "assets/images/ic_home_nav_bill.png",
+    "assets/images/ic_home_nav_bill_pressed.png"
+  ],
+  "报表": [
+    "assets/images/ic_home_nav_chart.png",
+    "assets/images/ic_home_nav_chart_pressed.png"
+  ],
+  "赚钱": [
+    "assets/images/ic_home_nav_finance.png",
+    "assets/images/ic_home_nav_finance_pressed.png"
+  ],
+  "我": [
+    "assets/images/ic_home_nav_me.png",
+    "assets/images/ic_home_nav_me_pressed.png"
+  ],
+};
+
+List test = [
+  Text("首页的页面"),
+  Text("账单的页面"),
+  Text("报表的页面"),
+  Text("赚钱的页面"),
+  Text("我的页面")
+];
+
+class FAbook extends StatefulWidget {
+  @override
+  _FAbookState createState() => _FAbookState();
+}
+
+class _FAbookState extends State<FAbook> {
+  int _currentIndex = 0;
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
+        home: Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text("我的记账app"),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+      body: Container(
+        child: test[_currentIndex],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: (int index) {
+          print(index);
+          setState(() {
+            _currentIndex = index;
+          });
+        },
+        currentIndex: _currentIndex,
+        selectedItemColor: _selectColor,
+        unselectedItemColor: _unselectColor,
+        selectedFontSize: 10.0,
+        unselectedFontSize: 10.0,
+        unselectedLabelStyle: TextStyle(
+          color: _unselectColor,
         ),
+        items: <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Image(
+              width: _selectIconSize,
+              height: _selectIconSize,
+              fit: BoxFit.cover,
+              image: AssetImage(
+                imageUrlMap["首页"][0],
+              ),
+            ),
+            label: "首页",
+          ),
+          BottomNavigationBarItem(
+            icon: Image(
+              width: _selectIconSize,
+              height: _selectIconSize,
+              fit: BoxFit.cover,
+              image: AssetImage(
+                imageUrlMap["账单"][0],
+              ),
+            ),
+            label: "账单",
+          ),
+          BottomNavigationBarItem(
+            icon: Image(
+              width: _selectIconSize,
+              height: _selectIconSize,
+              fit: BoxFit.cover,
+              image: AssetImage(
+                imageUrlMap["报表"][0],
+              ),
+            ),
+            label: "报表",
+          ),
+          BottomNavigationBarItem(
+            icon: Image(
+              width: _selectIconSize,
+              height: _selectIconSize,
+              fit: BoxFit.cover,
+              image: AssetImage(
+                imageUrlMap["赚钱"][0],
+              ),
+            ),
+            label: "赚钱",
+          ),
+          BottomNavigationBarItem(
+            icon: Image(
+              width: _selectIconSize,
+              height: _selectIconSize,
+              fit: BoxFit.cover,
+              image: AssetImage(
+                imageUrlMap["我"][0],
+              ),
+            ),
+            label: "我",
+          ),
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+    ));
   }
 }
